99cd7e842bee71f895cc6e67aef31c4c6aa6f57b
Jack W. Tat <jackwtat@gmail.com>
2017-10-10 15:51:03 -0400

    Big stuff

---
+++ app/src/main/java/jackwtat/simplembta/Fragments/UpdatableFragment.java
@@ -0,0 +1,11 @@
+package jackwtat.simplembta.Fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by jackw on 10/10/2017.
+ */
+
+public abstract class UpdatableFragment extends Fragment {
+    public abstract void update();
+}
