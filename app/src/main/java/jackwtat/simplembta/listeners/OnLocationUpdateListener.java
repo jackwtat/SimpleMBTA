740c6967dad298e3f9aba47377e9ea9598668c6a
Jack W. Tat <jackwtat@gmail.com>
2017-11-28 14:43:23 -0500

    Fixed typo

--- app/src/main/java/jackwtat/simplembta/listeners/OnLocationUpdateListener.java
+++ app/src/main/java/jackwtat/simplembta/listeners/OnLocationUpdateListener.java
@@ -6,6 +6,6 @@ import android.location.Location;
  * Created by jackw on 11/27/2017.
  */
 
-public interface LocationUpdateListener {
+public interface OnLocationUpdateListener {
     void onLocationUpdate(Location location);
 }
