9eee120087f88ee3322f27433a14ce8e0eb9e27f
Jack W. Tat <jackwtat@gmail.com>
2018-05-22 06:19:52 -0400

    Prediction times and destinations now dynamically added

--- app/src/main/java/jackwtat/simplembta/adapters/PredictionsListAdapter.java
+++ app/src/main/java/jackwtat/simplembta/adapters/PredictionsListAdapter.java
@@ -10,11 +10,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.graphics.Color;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
+import jackwtat.simplembta.views.PredictionView;
 import jackwtat.simplembta.R;
 import jackwtat.simplembta.mbta.structure.Mode;
 import jackwtat.simplembta.mbta.structure.Prediction;
@@ -49,92 +52,88 @@ public class PredictionsListAdapter extends ArrayAdapter<ArrayList<Prediction>> 
             return listItemView;
         }
 
-        // Get first and second prediction
-        Prediction p1 = predictions.get(0);
-
         // Initialize all the views
-        TextView routeName = listItemView.findViewById(R.id.route_text_view);
-        TextView stopName = listItemView.findViewById(R.id.stop_text_view);
-        TextView alertIndicator = listItemView.findViewById(R.id.alert_indicator_text_view);
-        TextView destination_1 = listItemView.findViewById(R.id.destination_text_view_1);
-        TextView destination_2 = listItemView.findViewById(R.id.destination_text_view_2);
-        TextView departureTime_1 = listItemView.findViewById(R.id.time_text_view_1);
-        TextView departureTime_2 = listItemView.findViewById(R.id.time_text_view_2);
-        TextView departureTime_3 = listItemView.findViewById(R.id.time_text_view_3);
-        TextView departureTime_4 = listItemView.findViewById(R.id.time_text_view_4);
-        TextView trainNumber_1 = listItemView.findViewById(R.id.train_text_view_1);
-        TextView trainNumber_2 = listItemView.findViewById(R.id.train_text_view_2);
+        LinearLayout predictionsLayout = listItemView.findViewById(R.id.predictions_layout);
+        TextView routeNameView = listItemView.findViewById(R.id.route_text_view);
+        TextView stopNameView = listItemView.findViewById(R.id.stop_text_view);
+        TextView alertIndicatorView = listItemView.findViewById(R.id.alert_indicator_text_view);
 
         // Hide the views that have optional values for now
-        alertIndicator.setVisibility(View.GONE);
-        destination_2.setVisibility(View.GONE);
-        departureTime_3.setVisibility(View.GONE);
-        departureTime_4.setVisibility(View.GONE);
-        trainNumber_1.setVisibility(View.GONE);
-        trainNumber_2.setVisibility(View.GONE);
+        alertIndicatorView.setVisibility(View.GONE);
+
+        // Clear previous predictions
+        if(predictionsLayout.getChildCount() > 0) {
+            predictionsLayout.removeAllViews();
+        }
+
+        // Get the route
+        Route route = predictions.get(0).getRoute();
 
         // Set the stop name
-        stopName.setText(p1.getStopName());
+        stopNameView.setText(predictions.get(0).getStopName());
 
         // Set the route name
-        setRouteView(p1.getRoute(), routeName);
+        setRouteView(route, routeNameView);
 
         // Set the indicator for service alerts
-        for (ServiceAlert alert : p1.getRoute().getServiceAlerts()) {
-            alertIndicator.setVisibility(View.VISIBLE);
+        for (ServiceAlert alert : route.getServiceAlerts()) {
+            alertIndicatorView.setVisibility(View.VISIBLE);
             if (alert.isActive() && (alert.getLifecycle() == ServiceAlert.Lifecycle.NEW || alert.getLifecycle() == ServiceAlert.Lifecycle.UNKNOWN)) {
-                alertIndicator.setText(getContext().getResources().getString(R.string.service_alert_urgent));
-                alertIndicator.setTextColor(ContextCompat.getColor(getContext(), R.color.ServiceAlert_Urgent));
+                alertIndicatorView.setText(getContext().getResources().getString(R.string.service_alert_urgent));
+                alertIndicatorView.setTextColor(ContextCompat.getColor(getContext(), R.color.ServiceAlert_Urgent));
                 break;
             } else {
-                alertIndicator.setText(getContext().getResources().getString(R.string.service_alert_advisory));
-                alertIndicator.setTextColor(ContextCompat.getColor(getContext(), R.color.ServiceAlert_Advisory));
+                alertIndicatorView.setText(getContext().getResources().getString(R.string.service_alert_advisory));
+                alertIndicatorView.setTextColor(ContextCompat.getColor(getContext(), R.color.ServiceAlert_Advisory));
             }
         }
 
-        // Set the destination and departure times of the predictions
-        if (predictions.size() > 1 && p1.getDepartureTime() != null) {
-            Prediction p2 = predictions.get(1);
+        // Display the destinations and prediction times using PredictionViews
+        if (route.getMode() != Mode.COMMUTER_RAIL) {
+            // Group predictions by destination in a HashMap
+            // Maintain the order of the destinations in an ArrayList
+            HashMap<String, ArrayList<Prediction>> pMap = new HashMap<>();
+            ArrayList<String> dList = new ArrayList<>();
 
-            if (p2.getTrip().getDestination().equals(p1.getTrip().getDestination()) &&
-                    p1.getRoute().getMode() != Mode.COMMUTER_RAIL) {
-                setPredictionViews(p1, p2, departureTime_1, departureTime_2, destination_1);
-            } else {
-                setPredictionViews(p1, null, departureTime_1, departureTime_2, destination_1);
-                setPredictionViews(p2, null, departureTime_3, departureTime_4, destination_2);
+            // Add the predictions to the HashMap and destinations to the ArrayList
+            for (Prediction p : predictions) {
+                String dest = p.getTrip().getDestination();
+                if (!dList.contains(dest)) {
+                    dList.add(dest);
+                    pMap.put(dest, new ArrayList<Prediction>());
+                }
+                pMap.get(dest).add(p);
+            }
+
+            // Create a new PredictionView for each group of predictions
+            for (String dest : dList) {
+                ArrayList<Prediction> pList = pMap.get(dest);
+                if (pList.size() > 1) {
+                    predictionsLayout.addView(new PredictionView(getContext(), pList.get(0), pList.get(1)));
+                } else {
+                    predictionsLayout.addView(new PredictionView(getContext(), pList.get(0)));
+                }
             }
         } else {
-            setPredictionViews(p1, null, departureTime_1, departureTime_2, destination_1);
-        }
-
-        // Set the train numbers if commuter rail
-        if (p1.getRoute().getMode() == Mode.COMMUTER_RAIL && !p1.getTrip().getName().equals("")) {
-            String tno = "(Train " + p1.getTrip().getName() + ")";
-            trainNumber_1.setText(tno);
-            trainNumber_1.setVisibility(View.VISIBLE);
-
-            if (predictions.size() > 1) {
-                Prediction p2 = predictions.get(1);
-                if (p2 != null && !p2.getTrip().getName().equals("")) {
-                    tno = "(Train " + p2.getTrip().getName() + ")";
-                    trainNumber_2.setText(tno);
-                    trainNumber_2.setVisibility(View.VISIBLE);
-                }
+            // Create the PredictionViews for the Commuter Rail
+            // Maximum 3 predictions to minimize scrolling
+            for (int i = 0; i < 3 && i < predictions.size(); i++) {
+                predictionsLayout.addView(new PredictionView(getContext(), predictions.get(i)));
             }
         }
 
         return listItemView;
     }
 
-    private void setRouteView(Route rte, TextView routeView) {
+    private void setRouteView(Route route, TextView routeView) {
         Drawable bkgd = getContext().getResources().getDrawable(R.drawable.route_background);
-        DrawableCompat.setTint(bkgd, Color.parseColor(rte.getColor()));
+        DrawableCompat.setTint(bkgd, Color.parseColor(route.getColor()));
 
         routeView.setBackground(bkgd);
-        routeView.setTextColor(Color.parseColor(rte.getTextColor()));
+        routeView.setTextColor(Color.parseColor(route.getTextColor()));
 
-        String routeId = rte.getId();
-        Mode mode = rte.getMode();
+        String routeId = route.getId();
+        Mode mode = route.getMode();
 
         if (mode == Mode.HEAVY_RAIL) {
             if (routeId.equals("Red"))
@@ -163,8 +162,8 @@ public class PredictionsListAdapter extends ArrayAdapter<ArrayList<Prediction>> 
         } else if (mode == Mode.BUS) {
             if (routeId.equals("746"))
                 routeView.setText(getContext().getResources().getString(R.string.silver_line_waterfront_short_name));
-            else if (!rte.getShortName().equals("") && !rte.getShortName().equals("null"))
-                routeView.setText(rte.getShortName());
+            else if (!route.getShortName().equals("") && !route.getShortName().equals("null"))
+                routeView.setText(route.getShortName());
             else
                 routeView.setText(routeId);
 
@@ -176,51 +175,6 @@ public class PredictionsListAdapter extends ArrayAdapter<ArrayList<Prediction>> 
 
         } else {
             routeView.setText(routeId);
-        }
-    }
-
-    private void setPredictionViews(Prediction prediction_1, @Nullable Prediction prediction_2,
-                                    TextView departureTimeView_1, TextView departureTimeView_2,
-                                    TextView destinationView) {
-        departureTimeView_1.setVisibility(View.VISIBLE);
-        departureTimeView_2.setVisibility(View.VISIBLE);
-        String dept_1;
-        String dept_2;
-        String dest;
-
-        if (prediction_1.getDepartureTime() != null) {
-            if (prediction_1.getTimeUntilDeparture() > 0) {
-                dept_1 = (prediction_1.getTimeUntilDeparture() / 60000) + "";
-            } else {
-                dept_1 = "0";
-            }
-
-            if (prediction_2 != null && prediction_2.getDepartureTime() != null) {
-                dept_1 = dept_1 + ",";
-                if (prediction_2.getTimeUntilDeparture() > 0) {
-                    dept_2 = (prediction_2.getTimeUntilDeparture() / 60000) + " min";
-                } else {
-                    dept_2 = "0 min";
-                }
-            } else {
-                dept_2 = "min";
-            }
-
-            departureTimeView_1.setText(dept_1);
-            departureTimeView_2.setText(dept_2);
-
-            dest = prediction_1.getTrip().getDestination();
-            destinationView.setText(dest);
-            destinationView.setVisibility(View.VISIBLE);
-
-        } else {
-            departureTimeView_1.setText("---");
-            departureTimeView_2.setText("");
-
-            if (destinationView != null) {
-                destinationView.setText(prediction_1.getTrip().getDestination());
-                destinationView.setVisibility(View.VISIBLE);
-            }
         }
     }
 }
---
+++ app/src/main/java/jackwtat/simplembta/views/PredictionView.java
@@ -0,0 +1,75 @@
+package jackwtat.simplembta.views;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import jackwtat.simplembta.R;
+import jackwtat.simplembta.mbta.structure.Mode;
+import jackwtat.simplembta.mbta.structure.Prediction;
+
+public class PredictionView extends LinearLayout {
+    View rootView;
+    TextView destinationView;
+    TextView trainNumberView;
+    TextView firstTimeView;
+    TextView secondTimeView;
+
+    public PredictionView(Context context, Prediction prediction) {
+        super(context);
+        setPrediction(context, prediction, null);
+    }
+
+    public PredictionView(Context context, Prediction prediction1, Prediction prediction2) {
+        super(context);
+        setPrediction(context, prediction1, prediction2);
+    }
+
+    private void setPrediction(Context context, Prediction p1, @Nullable Prediction p2) {
+        rootView = inflate(context, R.layout.prediction_view, this);
+        destinationView = rootView.findViewById(R.id.destination_text_view);
+        trainNumberView = rootView.findViewById(R.id.train_number_text_view);
+        firstTimeView = rootView.findViewById(R.id.first_time_text_view);
+        secondTimeView = rootView.findViewById(R.id.second_time_text_view);
+
+        destinationView.setText(p1.getTrip().getDestination());
+
+        if (p1.getRoute().getMode() == Mode.COMMUTER_RAIL && !p1.getTrip().getName().equals("")) {
+            String trainNumber = "(" + context.getResources().getString(R.string.train) + " " +
+                    p1.getTrip().getName() + ")";
+            trainNumberView.setText(trainNumber);
+        } else {
+            trainNumberView.setVisibility(GONE);
+        }
+
+        if (p1.getDepartureTime() != null) {
+            String dept_1;
+            String dept_2;
+
+            if (p1.getTimeUntilDeparture() > 0) {
+                dept_1 = (p1.getTimeUntilDeparture() / 60000) + "";
+            } else {
+                dept_1 = "0";
+            }
+
+            if (p2 != null && p2.getDepartureTime() != null) {
+                dept_1 = dept_1 + ",";
+                if (p2.getTimeUntilDeparture() > 0) {
+                    dept_2 = (p2.getTimeUntilDeparture() / 60000) + " min";
+                } else {
+                    dept_2 = "0 min";
+                }
+            } else {
+                dept_2 = "min";
+            }
+
+            firstTimeView.setText(dept_1);
+            secondTimeView.setText(dept_2);
+        } else {
+            firstTimeView.setText("---");
+            secondTimeView.setVisibility(GONE);
+        }
+    }
+}
