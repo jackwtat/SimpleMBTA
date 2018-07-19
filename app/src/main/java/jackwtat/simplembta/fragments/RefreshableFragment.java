9eb644c44f68de48ec539e7dc68ad0ad98e4f0a5
Jack W. Tat <jackwtat@gmail.com>
2018-07-19 05:19:37 -0400

    Fixed refresh bug

--- app/src/main/java/jackwtat/simplembta/fragments/RefreshableFragment.java
+++ app/src/main/java/jackwtat/simplembta/fragments/RefreshableFragment.java
@@ -5,5 +5,7 @@ import android.support.v4.app.Fragment;
 public abstract class RefreshableFragment extends Fragment {
     public abstract void refresh();
 
+    public abstract void autoRefresh();
+
     public abstract void forceRefresh();
 }
