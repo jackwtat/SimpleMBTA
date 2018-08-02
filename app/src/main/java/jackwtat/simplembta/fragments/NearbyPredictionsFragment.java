9622fb6e912abfaea6308524fb85a12d6b89f186
Jack W. Tat <jackwtat@gmail.com>
2018-08-01 22:16:57 -0400

    Converted ListView to RecyclerView

--- app/src/main/java/jackwtat/simplembta/fragments/NearbyPredictionsFragment.java
+++ app/src/main/java/jackwtat/simplembta/fragments/NearbyPredictionsFragment.java
@@ -1,11 +1,15 @@
 package jackwtat.simplembta.fragments;
 
-import android.content.pm.PackageManager;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,18 +19,17 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import jackwtat.simplembta.R;
+import jackwtat.simplembta.adapters.PredictionsAdapter;
 import jackwtat.simplembta.controllers.NearbyPredictionsController;
 import jackwtat.simplembta.controllers.listeners.OnLocationErrorListener;
 import jackwtat.simplembta.controllers.listeners.OnNetworkErrorListener;
 import jackwtat.simplembta.controllers.listeners.OnPostExecuteListener;
 import jackwtat.simplembta.controllers.listeners.OnProgressUpdateListener;
+import jackwtat.simplembta.mbta.structure.Prediction;
+import jackwtat.simplembta.mbta.structure.Route;
 import jackwtat.simplembta.mbta.structure.Stop;
-import jackwtat.simplembta.views.PredictionsListView;
-
-
-/**
- * Created by jackw on 8/21/2017.
- */
+import jackwtat.simplembta.views.AlertsListView;
+import jackwtat.simplembta.views.RouteNameView;
 
 public class NearbyPredictionsFragment extends RefreshableFragment {
     private final static String LOG_TAG = "NearbyPredsFragment";
@@ -34,9 +37,12 @@ public class NearbyPredictionsFragment extends RefreshableFragment {
     private final long AUTO_REFRESH_RATE = 45000;
 
     private View rootView;
-    private PredictionsListView predictionsListView;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private RecyclerView recyclerView;
+    private AlertDialog alertDialog;
 
     private NearbyPredictionsController controller;
+    private PredictionsAdapter predictionsAdapter;
     private Timer autoRefreshTimer;
 
     private boolean resetUI = false;
@@ -48,30 +54,32 @@ public class NearbyPredictionsFragment extends RefreshableFragment {
         controller = new NearbyPredictionsController(getContext(),
                 new OnPostExecuteListener() {
                     public void onPostExecute(List<Stop> stops) {
-                        try {
-                            predictionsListView.publishPredictions(getContext(), stops, resetUI);
-                        } catch (IllegalStateException e) {
-                            Log.e(LOG_TAG, "Pushing get results to nonexistent view");
+                        predictionsAdapter.setPredictions(Prediction.getUniqueSortedPredictions(stops));
+
+                        swipeRefreshLayout.setRefreshing(false);
+
+                        if (resetUI) {
+                            recyclerView.scrollToPosition(0);
+
+                            if (alertDialog != null) {
+                                alertDialog.dismiss();
+                            }
                         }
                     }
                 },
                 new OnProgressUpdateListener() {
                     public void onProgressUpdate(int progress) {
-                        try {
-                            predictionsListView.setRefreshProgress();
-                        } catch (IllegalStateException e) {
-                            Log.e(LOG_TAG, "Pushing progress update to nonexistent view");
-                        }
+                        swipeRefreshLayout.setRefreshing(true);
                     }
                 },
                 new OnNetworkErrorListener() {
                     public void onNetworkError() {
-                        predictionsListView.onRefreshError(getResources().getString(R.string.no_network_connectivity));
+                        swipeRefreshLayout.setRefreshing(false);
                     }
                 },
                 new OnLocationErrorListener() {
                     public void onLocationError() {
-                        predictionsListView.onRefreshError(getResources().getString(R.string.no_location));
+                        swipeRefreshLayout.setRefreshing(false);
                     }
                 });
     }
@@ -81,13 +89,53 @@ public class NearbyPredictionsFragment extends RefreshableFragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_nearby_predictions, container, false);
 
-        predictionsListView = rootView.findViewById(R.id.predictions_list_view);
-        predictionsListView.setOnSwipeRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+        swipeRefreshLayout = rootView.findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setColorSchemeColors(ContextCompat.getColor(getContext(),
+                R.color.colorAccent));
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
                 forceRefresh();
             }
         });
+
+        recyclerView = rootView.findViewById(R.id.predictions_recycler_view);
+
+        GridLayoutManager glm = new GridLayoutManager(getContext(), 1);
+        recyclerView.setLayoutManager(glm);
+
+        predictionsAdapter = new PredictionsAdapter();
+        predictionsAdapter.setOnItemClickListener(new PredictionsAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int i) {
+                Route route = predictionsAdapter.getRoute(i);
+
+                if (route.hasServiceAlerts()) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+                    RouteNameView routeNameView = new RouteNameView(getContext(), route,
+                            getContext().getResources().getDimension(R.dimen.large_route_name_text_size), RouteNameView.SQUARE_BACKGROUND,
+                            false, true);
+                    routeNameView.setGravity(Gravity.CENTER);
+
+                    builder.setCustomTitle(routeNameView);
+
+                    builder.setView(new AlertsListView(getContext(), route.getServiceAlerts()));
+
+                    builder.setPositiveButton(getResources().getString(R.string.dialog_close_button), new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            alertDialog.dismiss();
+                        }
+                    });
+
+                    alertDialog = builder.create();
+                    alertDialog.show();
+                }
+            }
+        });
+
+        recyclerView.setAdapter(predictionsAdapter);
 
         return rootView;
     }
@@ -115,6 +163,7 @@ public class NearbyPredictionsFragment extends RefreshableFragment {
         super.onResume();
 
         controller.connect();
+
         refresh();
     }
 
@@ -122,21 +171,20 @@ public class NearbyPredictionsFragment extends RefreshableFragment {
     public void onPause() {
         super.onPause();
 
-        // Hide alert dialog
-        predictionsListView.hideAlertsDialog();
-
+        if (alertDialog != null) {
+            alertDialog.hide();
+        }
     }
 
     @Override
     public void onStop() {
-        if (controller.isRunning()) {
-            predictionsListView.onRefreshCanceled();
-        }
+        super.onStop();
 
-        autoRefreshTimer.cancel();
         controller.cancel();
 
-        super.onStop();
+        autoRefreshTimer.cancel();
+
+        swipeRefreshLayout.setRefreshing(false);
     }
 
     @Override
