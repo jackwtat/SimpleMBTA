f71c301e3ece056cb8ab454d96ffcb68528c7c20
Jack W. Tat <jackwtat@gmail.com>
2018-11-20 18:14:03 -0500

    New manual/list search feature UI elements

---
+++ app/src/main/java/jackwtat/simplembta/adapters/spinners/RoutesSpinnerAdapter.java
@@ -0,0 +1,79 @@
+package jackwtat.simplembta.adapters.spinners;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import jackwtat.simplembta.R;
+import jackwtat.simplembta.model.routes.Route;
+import jackwtat.simplembta.views.RouteNameView;
+
+public class RoutesSpinnerAdapter extends ArrayAdapter<Route> {
+    Context context;
+    Route[] routes;
+    Route selectedRoute;
+
+    public RoutesSpinnerAdapter(Context context, Route[] routes) {
+        super(context, 0, routes);
+        this.context = context;
+        this.routes = routes;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return createItemView(routes[position], parent, R.layout.spinner_item_route_selected);
+    }
+
+    @Override
+    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItem;
+        Route route = routes[position];
+
+        if (route.equals(selectedRoute)) {
+            listItem = createItemView(route, parent, R.layout.spinner_item_route_dropdown_selected);
+        } else {
+            listItem = createItemView(route, parent, R.layout.spinner_item_route_dropdown);
+        }
+
+        return listItem;
+    }
+
+    private View createItemView(Route route, @NonNull ViewGroup parent, @NonNull int layout) {
+        View listItem = LayoutInflater.from(context).inflate(layout, parent, false);
+
+        RouteNameView nameTextView = listItem.findViewById(R.id.route_name_view);
+
+        TextView routeDescriptionTextView = listItem.findViewById(R.id.route_description_text_view);
+
+        if (route != null) {
+            nameTextView.setRouteNameView(context, route);
+
+            if (route.getMode() == Route.BUS) {
+                routeDescriptionTextView.setText(route.getLongName());
+                routeDescriptionTextView.setVisibility(View.VISIBLE);
+            } else {
+                routeDescriptionTextView.setVisibility(View.GONE);
+            }
+        } else {
+            nameTextView.setRouteNameView(context, new Route("null"));
+        }
+
+        return listItem;
+    }
+
+    @Nullable
+    @Override
+    public Route getItem(int position) {
+        return routes[position];
+    }
+
+    public void setSelectedRoute(Route route) {
+        selectedRoute = route;
+    }
+}
---
+++ app/src/main/java/jackwtat/simplembta/views/ManualSearchSpinners.java
@@ -0,0 +1,157 @@
+package jackwtat.simplembta.views;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+
+import jackwtat.simplembta.R;
+import jackwtat.simplembta.adapters.spinners.DirectionsSpinnerAdapter;
+import jackwtat.simplembta.adapters.spinners.RoutesSpinnerAdapter;
+import jackwtat.simplembta.adapters.spinners.StopsSpinnerAdapter;
+import jackwtat.simplembta.model.Direction;
+import jackwtat.simplembta.model.Stop;
+import jackwtat.simplembta.model.routes.Route;
+
+public class ManualSearchSpinners extends LinearLayout implements AdapterView.OnItemSelectedListener {
+    private View rootView;
+    private Spinner routeSpinner;
+    private Spinner directionSpinner;
+    private Spinner stopSpinner;
+
+    private RoutesSpinnerAdapter routesAdapter;
+    private DirectionsSpinnerAdapter directionsAdapter;
+    private StopsSpinnerAdapter stopsAdapter;
+
+    private Route[] routes = {};
+    private Direction[] directions = {};
+    private Stop[] stops = {};
+
+    private OnRouteSelectedListener onRouteSelectedListener;
+    private OnDirectionSelectedListener onDirectionSelectedListener;
+    private OnStopSelectedListener onStopSelectedListener;
+
+    public ManualSearchSpinners(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public ManualSearchSpinners(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public ManualSearchSpinners(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        rootView = inflate(context, R.layout.spinners_manual_search, this);
+
+        routeSpinner = rootView.findViewById(R.id.route_spinner);
+        routeSpinner.setOnItemSelectedListener(this);
+
+        directionSpinner = rootView.findViewById(R.id.direction_spinner);
+        directionSpinner.setOnItemSelectedListener(this);
+
+        stopSpinner = rootView.findViewById(R.id.stop_spinner);
+        stopSpinner.setOnItemSelectedListener(this);
+    }
+
+    public void populateRouteSpinner(Route[] routes) {
+        this.routes = routes;
+        routesAdapter = new RoutesSpinnerAdapter(getContext(), routes);
+        routeSpinner.setAdapter(routesAdapter);
+    }
+
+    public void populateDirectionSpinner(Direction[] directions) {
+        this.directions = directions;
+        directionsAdapter = new DirectionsSpinnerAdapter(getContext(), directions);
+        directionSpinner.setAdapter(directionsAdapter);
+    }
+
+    public void populateStopSpinner(Stop[] stops) {
+        this.stops = stops;
+        stopsAdapter = new StopsSpinnerAdapter(getContext(), stops);
+        stopSpinner.setAdapter(stopsAdapter);
+    }
+
+    public void selectedRoute(String routeId) {
+        for (int i = 0; i < routes.length; i++) {
+            if (routes[i].getId().equals(routeId)) {
+                routeSpinner.setSelection(i);
+            }
+        }
+    }
+
+    public void selectDirection(int directionId) {
+        for (int i = 0; i < directions.length; i++) {
+            if (directions[i].getId() == directionId) {
+                directionSpinner.setSelection(i);
+            }
+        }
+    }
+
+    public void selectStop(String stopId) {
+        for (int i = 0; i < stops.length; i++) {
+            if (stops[i].getId().equals(stopId)) {
+                stopSpinner.setSelection(i);
+                break;
+            }
+        }
+    }
+
+    public void setOnRouteSelectedListener(OnRouteSelectedListener onRouteSelectedListener) {
+        this.onRouteSelectedListener = onRouteSelectedListener;
+    }
+
+    public void setOnDirectionSelectedListener(OnDirectionSelectedListener onDirectionSelectedListener) {
+        this.onDirectionSelectedListener = onDirectionSelectedListener;
+    }
+
+    public void setOnStopSelectedListener(OnStopSelectedListener onStopSelectedListener) {
+        this.onStopSelectedListener = onStopSelectedListener;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (parent.getId()) {
+            case R.id.route_spinner:
+                Route selectedRoute = (Route) parent.getItemAtPosition(position);
+                routesAdapter.setSelectedRoute(selectedRoute);
+                onRouteSelectedListener.onRouteSelected(selectedRoute);
+                break;
+            case R.id.direction_spinner:
+                Direction selectedDirection = (Direction) parent.getItemAtPosition(position);
+                directionsAdapter.setSelectedDirection(selectedDirection);
+                onDirectionSelectedListener.onDirectionSelected(selectedDirection);
+                break;
+            case R.id.stop_spinner:
+                Stop selectedStop = (Stop) parent.getItemAtPosition(position);
+                stopsAdapter.setSelectedStop(selectedStop);
+                onStopSelectedListener.onStopSelected(selectedStop);
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    public interface OnRouteSelectedListener {
+        void onRouteSelected(Route route);
+    }
+
+    public interface OnDirectionSelectedListener {
+        void onDirectionSelected(Direction direction);
+    }
+
+    public interface OnStopSelectedListener {
+        void onStopSelected(Stop stop);
+    }
+}
