823b4af0199aa60a7e851d3d506adb8482ed7fe7
jackwtat <jackwtat@gmail.com>
2019-08-10 22:55:46 -0400

    Bifurcate Map Search query to improve performance

--- app/src/main/java/jackwtat/simplembta/asyncTasks/PredictionsByStopsAsyncTask.java
+++ app/src/main/java/jackwtat/simplembta/asyncTasks/PredictionsByStopsAsyncTask.java
@@ -1,6 +1,9 @@
 package jackwtat.simplembta.asyncTasks;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import jackwtat.simplembta.clients.RealTimeApiClient;
 import jackwtat.simplembta.jsonParsers.PredictionsJsonParser;
 import jackwtat.simplembta.model.Prediction;
@@ -20,24 +23,19 @@ public class PredictionsByStopsAsyncTask extends PredictionsAsyncTask {
 
     @Override
     protected Prediction[] doInBackground(Void... voids) {
+        ArrayList<Prediction> predictions = new ArrayList<>();
         RealTimeApiClient realTimeApiClient = new RealTimeApiClient(realTimeApiKey);
 
         StringBuilder stopArgBuilder = new StringBuilder();
         for (String stopId : stopIds) {
             stopArgBuilder.append(stopId).append(",");
         }
+        String stops = stopArgBuilder.toString();
 
-        String[] predictionsArgs = {
-                "filter[stop]=" + stopArgBuilder.toString(),
-                "include=route,trip,stop,schedule,vehicle"
-        };
+        predictions.addAll(makeQuery(realTimeApiClient, stops, "0,1"));
+        predictions.addAll(makeQuery(realTimeApiClient, stops, "2,3,4"));
 
-        String jsonResponse = realTimeApiClient.get("predictions", predictionsArgs);
-
-        if (jsonResponse != null)
-            return PredictionsJsonParser.parse(jsonResponse);
-        else
-            return null;
+        return predictions.toArray(new Prediction[0]);
     }
 
     @Override
@@ -46,5 +44,20 @@ public class PredictionsByStopsAsyncTask extends PredictionsAsyncTask {
             onPostExecuteListener.onSuccess(predictions, true);
         else
             onPostExecuteListener.onError();
+    }
+
+    private ArrayList<Prediction> makeQuery(RealTimeApiClient client, String stops, String routeTypes) {
+        String[] predictionsArgs = {
+                "filter[stop]=" + stops,
+                "filter[route_type]=" + routeTypes,
+                "include=route,trip,stop,schedule,vehicle"
+        };
+
+        String jsonResponse = client.get("predictions", predictionsArgs);
+
+        if (jsonResponse != null)
+            return new ArrayList<>(Arrays.asList(PredictionsJsonParser.parse(jsonResponse)));
+        else
+            return new ArrayList<>();
     }
 }
